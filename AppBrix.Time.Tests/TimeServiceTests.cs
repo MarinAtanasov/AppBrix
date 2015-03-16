bc61260eecee8a52cfc1f3589c763d528e64c890
MarinAtanasov <marin.atanasov.bg@gmail.com>
2015-03-16 23:12:50 +0200

    Added 2 tests for Utc and Local ITimeService.

---
+++ AppBrix.Time.Tests/TimeServiceTests.cs
@@ -0,0 +1,58 @@
+// Copyright (c) MarinAtanasov. All rights reserved.
+// Licensed under the MIT License (MIT). See License.txt in the project root for license information.
+//
+using AppBrix.Application;
+using AppBrix.Configuration.Memory;
+using AppBrix.Resolve;
+using AppBrix.Tests;
+using AppBrix.Time.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace AppBrix.Time.Tests
+{
+    [TestClass]
+    public class TimeServiceTests
+    {
+        [TestMethod]
+        public void TestUtcTimeService()
+        {
+            var app = this.CreateAppWithTimeModule();
+            app.Start();
+            app.GetConfig<TimeConfig>().Kind = DateTimeKind.Utc;
+            app.Reinitialize();
+            var timeBefore = DateTime.UtcNow;
+            var time = app.GetTime();
+            var timeAfter = DateTime.UtcNow;
+            Assert.IsTrue(timeBefore <= time, "Before > call");
+            Assert.IsTrue(time.Kind == DateTimeKind.Utc, "Kind is not Utc");
+            Assert.IsTrue(timeAfter >= time, "After < call");
+            app.Stop();
+        }
+
+        [TestMethod]
+        public void TestLocalTimeService()
+        {
+            var app = this.CreateAppWithTimeModule();
+            app.Start();
+            app.GetConfig<TimeConfig>().Kind = DateTimeKind.Local;
+            app.Reinitialize();
+            var timeBefore = DateTime.Now;
+            var time = app.GetTime();
+            var timeAfter = DateTime.Now;
+            Assert.IsTrue(timeBefore <= time, "Before > call");
+            Assert.IsTrue(time.Kind == DateTimeKind.Local, "Kind is not Local");
+            Assert.IsTrue(timeAfter >= time, "After < call");
+            app.Stop();
+        }
+
+        private IApp CreateAppWithTimeModule()
+        {
+            return TestsUtils.CreateTestApp(
+                typeof(ResolverModule),
+                typeof(MemoryConfigModule),
+                typeof(TimeModule));
+        }
+    }
+}
