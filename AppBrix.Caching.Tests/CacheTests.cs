3293947e0c5110d923add9d1e238c77d1744fbd5
MarinAtanasov <marin.atanasov.bg@gmail.com>
2015-03-21 00:22:05 +0200

    Added 3 tests for cache module.

---
+++ AppBrix.Caching.Tests/CacheTests.cs
@@ -0,0 +1,86 @@
+﻿// Copyright (c) MarinAtanasov. All rights reserved.
+// Licensed under the MIT License (MIT). See License.txt in the project root for license information.
+//
+using AppBrix.Application;
+using AppBrix.Resolve;
+using AppBrix.Tests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppBrix.Caching.Tests
+{
+    [TestClass]
+    public class CacheTests
+    {
+        #region Setup and cleanup
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            CacheTests.app = TestsUtils.CreateTestApp(
+                typeof(ResolverModule),
+                typeof(CacheModule));
+            CacheTests.app.Start();
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            CacheTests.app.Stop();
+            CacheTests.app = null;
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            CacheTests.app.Reinitialize();
+        }
+        #endregion
+
+        #region Tests
+        [TestMethod]
+        public void TestGetCache()
+        {
+            var cache = CacheTests.app.GetCache();
+            Assert.IsNotNull(cache, "No cache found.");
+        }
+
+        [TestMethod]
+        public void TestCacheItem()
+        {
+            var cache = CacheTests.app.GetCache();
+            var key = "key";
+            var value = new object();
+            cache[key] = value;
+            Assert.IsTrue(cache.Contains(key), "Item not found.");
+            Assert.AreSame(value, cache[key], "Different object returned.");
+            cache.Remove(key);
+            Assert.IsFalse(cache.Contains(key), "Item not removed.");
+        }
+
+        [TestMethod]
+        public void TestReplaceItem()
+        {
+            var cache = CacheTests.app.GetCache();
+            var key = "key";
+            var value = new object();
+            cache[key] = value;
+            Assert.IsTrue(cache.Contains(key), "Item not found.");
+            Assert.AreSame(value, cache[key], "Different object returned.");
+            value = new object();
+            cache[key] = value;
+            Assert.IsTrue(cache.Contains(key), "Item not found after replace.");
+            Assert.AreSame(value, cache[key], "Different object returned after replace.");
+            cache.Remove(key);
+            Assert.IsFalse(cache.Contains(key), "Item not removed.");
+        }
+        #endregion
+
+        #region Private fields and constants
+        private static IApp app;
+        #endregion
+    }
+}
