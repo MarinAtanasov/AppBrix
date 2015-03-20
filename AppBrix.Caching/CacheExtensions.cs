1e69d75f8f8752799b287d8188d894f0c3afc648
MarinAtanasov <marin.atanasov.bg@gmail.com>
2015-03-21 00:09:09 +0200

    Added missing caching module files.

---
+++ AppBrix.Caching/CacheExtensions.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) MarinAtanasov. All rights reserved.
+// Licensed under the MIT License (MIT). See License.txt in the project root for license information.
+//
+using AppBrix.Application;
+using System;
+using System.Linq;
+using System.Runtime.Caching;
+
+namespace AppBrix
+{
+    public static class CacheExtensions
+    {
+        /// <summary>
+        /// Gets the currently registered cache.
+        /// </summary>
+        /// <param name="app">The currently running application.</param>
+        /// <returns>The cache.</returns>
+        public static ObjectCache GetCache(this IApp app)
+        {
+            return app.Get<ObjectCache>();
+        }
+
+        /// <summary>
+        /// Gets the item from the cache and casts it to the desired type.
+        /// Return null if the item is not found.
+        /// </summary>
+        /// <typeparam name="T">The type of the item.</typeparam>
+        /// <param name="cache">The cache.</param>
+        /// <param name="key">The key of the cached item.</param>
+        /// <param name="regionName">The region of the item. Optional.</param>
+        /// <returns>The cached item. Null if not found.</returns>
+        public static T Get<T>(this ObjectCache cache, string key, string regionName = null)
+            where T : class
+        {
+            return (T)cache.Get(key, regionName);
+        }
+    }
+}
---
+++ AppBrix.Caching/CacheModule.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) MarinAtanasov. All rights reserved.
+// Licensed under the MIT License (MIT). See License.txt in the project root for license information.
+//
+using AppBrix.Lifecycle;
+using AppBrix.Modules;
+using System;
+using System.Linq;
+using System.Runtime.Caching;
+
+namespace AppBrix.Caching
+{
+    public sealed class CacheModule : ModuleBase
+    {
+        #region Properties
+        public override int LoadPriority
+        {
+            get
+            {
+                return (int)ModuleLoadPriority.Config;
+            }
+        }
+        #endregion
+
+        #region Public and overriden methods
+        protected override void InitializeModule(IInitializeContext context)
+        {
+            this.App.Resolver.Register(this);
+            this.cache = new MemoryCache(this.App.Id.ToString());
+            this.App.Resolver.Register(this.cache);
+        }
+
+        protected override void UninitializeModule()
+        {
+            this.cache.Dispose();
+            this.cache = null;
+        }
+        #endregion
+
+        #region Private fields and constants
+        private MemoryCache cache;
+        #endregion
+    }
+}
