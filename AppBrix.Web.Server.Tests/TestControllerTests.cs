8df30114a86718e0c01b642ac54421f83507cb07
MarinAtanasov <marin.atanasov.bg@gmail.com>
2015-11-03 23:55:48 +0200

    Added a test which shows example of 2 apps with Web Api services running at the same time.

--- AppBrix.Web.Server.Tests/TestControllerTests.cs
+++ AppBrix.Web.Server.Tests/TestControllerTests.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) MarinAtanasov. All rights reserved.
 // Licensed under the MIT License (MIT). See License.txt in the project root for license information.
 //
+using AppBrix.Application;
 using AppBrix.Tests;
 using FluentAssertions;
 using Microsoft.AspNet.Builder;
@@ -25,7 +26,7 @@ namespace AppBrix.Web.Server.Tests
         [Fact]
         public async void TestConnection()
         {
-            using (var server = this.CreateTestServer())
+            using (var server = this.CreateTestServer(TestControllerTests.ServerBaseUrl))
             using (var client = server.CreateClient())
             {
                 var response = await client.GetAsync(TestControllerTests.TestConnectionServiceUrl);
@@ -34,31 +35,55 @@ namespace AppBrix.Web.Server.Tests
                 result.Should().BeTrue("this is the expected result when testing the connection");
             }
         }
+        
+        [Fact]
+        public async void TestConnectionBetweenTwoApps()
+        {
+            using (var server1 = this.CreateTestServer(TestControllerTests.ServerBaseUrl))
+            using (var client1 = server1.CreateClient())
+            using (var server2 = this.CreateTestServer(TestControllerTests.ServerBaseUrl))
+            using (var client2 = server2.CreateClient())
+            {
+                var response1 = await client1.GetAsync(TestControllerTests.TestConnectionService2Url);
+                response1.StatusCode.Should().Be(HttpStatusCode.OK, "the first app's call should reach the second app's service");
+                var result1 = bool.Parse(await response1.Content.ReadAsStringAsync());
+                result1.Should().BeTrue("this is the expected result when testing the connection from the first to the second app");
+
+                var response2 = await client2.GetAsync(TestControllerTests.TestConnectionServiceUrl);
+                response2.StatusCode.Should().Be(HttpStatusCode.OK, "the second app's call should reach the first app's service");
+                var result2 = bool.Parse(await response2.Content.ReadAsStringAsync());
+                result2.Should().BeTrue("this is the expected result when testing the connection from the first to the second app");
+            }
+        }
         #endregion
 
         #region Private methods
-        private TestServer CreateTestServer()
+        private TestServer CreateTestServer(Uri baseAddress, IApp app = null)
         {
-            Action<IApplicationBuilder> app = builder =>
+            Action<IApplicationBuilder> application = builder =>
             {
                 builder.UseMvc();
             };
             Action<IServiceCollection> services = collection =>
             {
                 collection.AddMvc();
-                collection.AddApp(TestUtils.CreateTestApp());
+                collection.AddApp(app ?? TestUtils.CreateTestApp());
             };
 
-            var server = TestServer.Create(app, services);
-            server.BaseAddress = this.ServerBaseUrl;
+            var server = TestServer.Create(application, services);
+            server.BaseAddress = baseAddress;
             return server;
         }
         #endregion
 
         #region Private fields and constants
         private const string ServerBaseAddress = "http://localhost:1337/";
-        private readonly Uri ServerBaseUrl = new Uri(TestControllerTests.ServerBaseAddress);
+        private static readonly Uri ServerBaseUrl = new Uri(TestControllerTests.ServerBaseAddress);
         private const string TestConnectionServiceUrl = TestControllerTests.ServerBaseAddress + "api/testconnection";
+
+        private const string Server2BaseAddress = "http://localhost:1338/";
+        private static readonly Uri Server2BaseUrl = new Uri(TestControllerTests.Server2BaseAddress);
+        private const string TestConnectionService2Url = TestControllerTests.Server2BaseAddress + "api/testconnection";
         #endregion
     }
 }
