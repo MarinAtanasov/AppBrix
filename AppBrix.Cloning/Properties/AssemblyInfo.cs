38165718ec3d8c91f17538c1c2d1af7d9cbbf534
MarinAtanasov <marin.atanasov.bg@gmail.com>
2015-11-04 01:17:24 +0200

    Removed unused friendly assembly references.

--- AppBrix.Cloning/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿// Copyright (c) MarinAtanasov. All rights reserved.
-// Licensed under the MIT License (MIT). See License.txt in the project root for license information.
-//
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("AppBrix.Cloning.Tests")]
