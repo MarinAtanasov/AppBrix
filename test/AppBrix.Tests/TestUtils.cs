295c62d518b96f7664e7ce7553e9c710ccf3c2ec
MarinAtanasov <marin.atanasov.bg@gmail.com>
2017-03-11 17:48:46 +0200

    Separated TestExtensions from TestUtils.

--- test/AppBrix.Tests/TestUtils.cs
+++ test/AppBrix.Tests/TestUtils.cs
@@ -4,11 +4,8 @@
 using AppBrix.Application;
 using AppBrix.Configuration;
 using AppBrix.Configuration.Memory;
-using FluentAssertions;
 using System;
-using System.Diagnostics;
 using System.Linq;
-using System.Threading;
 
 namespace AppBrix.Tests
 {
@@ -32,29 +29,6 @@ namespace AppBrix.Tests
                 config.Modules.Add(ModuleConfigElement.Create(module));
             }
             return App.Create(manager);
-        }
-        
-        /// <summary>
-        /// Asserts that a function will return a predefined value by a certain timeout.
-        /// </summary>
-        /// <typeparam name="T">The type of the result.</typeparam>
-        /// <param name="func">The function to be called until it returns the predefined value or the timeout expires.</param>
-        /// <param name="result">The result which should be returned by the function before the timeout expires.</param>
-        /// <param name="timeout">The timeout.</param>
-        /// <param name="because">A formatted phrase explaining why the assertion should be satisfied. If the phrase does not start with the word because, it is prepended to the message.</param>
-        /// <param name="becauseArgs">Zero or more values to use for filling in any <see cref="string.Format(string, object[])"/> compatible placeholders.</param>
-        public static void ShouldReturn<T>(this Func<T> func, T result, TimeSpan timeout, string because = "", params object[] becauseArgs)
-        {
-            if (func == null)
-                throw new ArgumentNullException(nameof(func));
-
-            var sw = Stopwatch.StartNew();
-            while (sw.Elapsed < timeout && !object.Equals(func(), result))
-            {
-                Thread.Sleep(1);
-            }
-
-            func().Should().Be(result, because, becauseArgs);
         }
         #endregion
     }
