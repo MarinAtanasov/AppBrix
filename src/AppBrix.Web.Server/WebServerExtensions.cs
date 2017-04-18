b4e0acce4568c1e585129fbe0d205598c7233381
MarinAtanasov <marin.atanasov.bg@gmail.com>
2017-04-18 19:53:14 +0300

    Refactored AddApp to be fluent api.

--- src/AppBrix.Web.Server/WebServerExtensions.cs
+++ src/AppBrix.Web.Server/WebServerExtensions.cs
@@ -21,13 +21,15 @@ namespace AppBrix
         /// </summary>
         /// <param name="services">The service collection.</param>
         /// <param name="app"></param>
-        public static void AddApp(this IServiceCollection services, IApp app)
+        /// <returns>The service collection.</returns>
+        public static IServiceCollection AddApp(this IServiceCollection services, IApp app)
         {
-            services.AddSingleton(app);
+            return services.AddSingleton(app);
         }
 
         /// <summary>
         /// Adds the currently registered <see cref="ILoggerProvider"/> to the logging system.
+        /// This method must be called from <see cref="M:Configure"/> method.
         /// </summary>
         /// <param name="loggerFactory">The logger factory where to add the provider.</param>
         /// <param name="app">The app where the provider has been registerd.</param>
