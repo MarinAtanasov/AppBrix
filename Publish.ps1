19659a25dc43b266f46a7fb9b81ab7520fbe4ac0
MarinAtanasov <marin.atanasov.bg@gmail.com>
2022-05-15 23:03:07 +0300

    Use dotnet nuget instead of nuget

--- Publish.ps1
+++ Publish.ps1
@@ -5,7 +5,7 @@ ForEach ($category in "Core", "Modules", "Bundles")
     ForEach ($folder in (Get-ChildItem -Path $category -Directory))
     {
         dotnet pack $folder.FullName --configuration Release --nologo /p:ContinuousIntegrationBuild=true;
-        nuget push "$($folder.FullName)\bin\Release\$($folder.Name).$($Version).nupkg";
-        nuget push "$($folder.FullName)\bin\Release\$($folder.Name).$($Version).snupkg";
+        dotnet nuget push "$($folder.FullName)\bin\Release\$($folder.Name).$($Version).nupkg";
+        dotnet nuget push "$($folder.FullName)\bin\Release\$($folder.Name).$($Version).snupkg";
     }
 }
